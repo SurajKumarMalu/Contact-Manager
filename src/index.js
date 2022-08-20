277c8c2a4084247d5a8f3733e98b7be5a7c07732
SurajKumarMalu <surajmalu0609@gmail.com>
2022-08-20 16:40:37 +0530

    3 files modified

--- src/index.js
+++ src/index.js
@@ -2,15 +2,35 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import '.style.css';
 
-import { createRoot } from 'react-dom/client';
+function Addmember(){
+ const [person,setPerson]=useState("");
+  
+    function handlechange(e){
+       setPerson(e.target.value);
+    }
+    function handleSubmit(e){
+      e.preventDefault();
+    }
+    return(
+      <form onSubmit= {handleSubmit}>
+        <input type="text" placeholder="Add New Contact" onChange={handleChange} value={person.name} />
+        <button type="Submit">Add</button>
+      </form>
+    );}
 
-import App from './App';
-
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    function People(props){
+      const arr=props.data;
+      const list=arr.map((val,index)=>
+      <li key={index}>{val}</li>
+      );
+      return <ul>{list}</ul>;
+    }
+const contacts=["Suraj","Kiran"];
+const el=(<div>
+     <Addmember />
+     <People data={contacts} />
+     </div>
+  );
+ReactDOM.render(
+  el,document.getElementById("root")
 );
